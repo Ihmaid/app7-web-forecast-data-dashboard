59dd0d587edaae51f704ef585378249a438bab50
Ihmaid <gabriel-ihmaid@ufrrj.br>
2024-06-10 11:18:05 -0300

    Plot a line graph and create one interaction, day 32

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
 
 st.title("Weather Forecast for the next days.")
 
@@ -14,3 +16,20 @@ data_view = st.selectbox(label="Select data to view:",
 
 if data_view:
     st.subheader(f"{data_view} for the next {number_days} days in {place}")
+
+
+def get_data(number_days):
+    dates = ["2022-10-25", "2022-10-30", "2022-11-05"]
+    temperatures = [20, 23, 27]
+    temperatures = [number_days * i for i in temperatures]
+    return dates, temperatures
+
+
+d, t = get_data(number_days)
+
+# Create a line graph
+figure = px.line(x=d,
+                 y=t,
+                 labels={"x": "Date", "y": "Temperature(C)"})
+# This streamlit widget receives a figure object from plotly library
+st.plotly_chart(figure)
