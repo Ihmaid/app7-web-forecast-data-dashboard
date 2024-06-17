9e682da2b596783a3a69569f47365554e680aa87
Ihmaid <gabriel-ihmaid@ufrrj.br>
2024-06-17 16:33:53 -0300

    Finished the app, day 33

--- main.py
+++ main.py
@@ -11,18 +11,33 @@ number_days = st.slider(label="Forecast Days:", min_value=1,
                         max_value=5, key="slider")
 
 data_view = st.selectbox(label="Select data to view:",
-                         options=["Temperature", "Sky"],
-                         placeholder=" ",
-                         index=None)
+                         options=["Temperature", "Sky"])
 
-if data_view:
-    st.subheader(f"{data_view} for the next {number_days} days in {place}")
+st.subheader(f"{data_view} for the next {number_days} days in {place}")
 
-x_axis, y_axis = get_data(place, number_days, data_view)
+if place:
+    try:
+        filtered_data = get_data(place, number_days)
 
-# Create a line graph
-figure = px.line(x=x_axis,
-                 y=y_axis,
-                 labels={"x": "Date", "y": "Temperature(C)"})
-# This streamlit widget receives a figure object from plotly library
-st.plotly_chart(figure)
+        if data_view == "Temperature":
+            temperatures = [item["main"]["temp"]/10 for item in filtered_data]
+            dates = [item["dt_txt"] for item in filtered_data]
+            # Create a line graph
+            figure = px.line(x=dates,
+                             y=temperatures,
+                             labels={"x": "Date", "y": "Temperature(C)"})
+            # This streamlit widget receives a figure object from plotly
+            # library
+            st.plotly_chart(figure)
+        else:
+            images = {"Clear": "images/clear.png",
+                      "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png",
+                      "Snow": "images/snow.png"}
+            sky_conditions = [item["weather"][0]["main"]
+                              for item in filtered_data]
+            images_path = [images[condition] for condition in sky_conditions]
+            st.image(images_path, width=115)
+
+    except KeyError:
+        st.text("Please enter a existing place!")
